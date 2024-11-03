e2c96374442929614754a1b0aadec2e30daa3cbe
zxhhhxz1221 <1305894626@qq.com>
2024-11-03 16:26:39 +0800

    Init

---
+++ src/main/java/io/github/zxh111222/sbbook/SbBookApplication.java
@@ -0,0 +1,13 @@
+package io.github.zxh111222.sbbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/zxh111222/sbbook/SbBookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zxh111222.sbbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
