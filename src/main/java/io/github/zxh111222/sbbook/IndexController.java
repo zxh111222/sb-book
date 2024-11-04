8a51ac7805afacdb30644f32de60cdc0c6ef8181
zxhhhxz1221 <1305894626@qq.com>
2024-11-04 20:03:30 +0800

    @RestController + @GetMapping("/") 处理首页的请求

---
+++ src/main/java/io/github/zxh111222/sbbook/IndexController.java
@@ -0,0 +1,12 @@
+package io.github.zxh111222.sbbook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
