dbe13d52ddb06011b2eeb4c0d7f756bcdfd28754
zxhhhxz1221 <1305894626@qq.com>
2024-11-04 20:30:12 +0800

    @RestController 改成 @Controller ...
    增加一个依赖 spring-boot-starter-thymeleaf
    此时的 return "index"; 是去 resources/templates 下的 index.html 页面

--- src/main/java/io/github/zxh111222/sbbook/IndexController.java
+++ src/main/java/io/github/zxh111222/sbbook/IndexController.java
@@ -1,9 +1,9 @@
 package io.github.zxh111222.sbbook;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
     @GetMapping("/")
     public String index() {
