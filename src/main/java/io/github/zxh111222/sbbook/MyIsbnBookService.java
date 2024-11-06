7387723c957d40aa994de69ac3d2eb3b2e0e4989
zxhhhxz1221 <1305894626@qq.com>
2024-11-06 20:25:07 +0800

    重构: Controller 中不再关心如何获取 Book， P1...
    黑盒子的思想
    先确定输入、输出

---
+++ src/main/java/io/github/zxh111222/sbbook/MyIsbnBookService.java
@@ -0,0 +1,10 @@
+package io.github.zxh111222.sbbook;
+
+import io.github.zxh111222.sbbook.pojo.Book;
+
+public class MyIsbnBookService {
+    public Book getBookByIsbn(String isbn) {
+
+        return null;
+    }
+}
